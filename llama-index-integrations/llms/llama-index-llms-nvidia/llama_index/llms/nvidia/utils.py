f898a8c25b8a6a4332801ac04574063382ef2f6d
Chris Alexiuk <calexiuk@nvidia.com>
2024-04-03 17:01:24 -0400

    Refactored playground to API Catalog

---
+++ llama-index-integrations/llms/llama-index-llms-nvidia/llama_index/llms/nvidia/utils.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+API_CATALOG_MODELS: Dict[str, int] = {
+    "mistralai/mistral-7b-instruct-v0.2",
+    "mistralai/mixtral-8x7b-instruct-v0.1",
+    "google/gemma-7b",
+    "meta/codellama-70b",
+    "meta/llama2-70b",
+    "cohere/aya-101",
+    "cohere/command-r"
+}
+
+def playground_modelname_to_contextsize(modelname: str) -> int:
+    if modelname not in API_CATALOG_MODELS:
+        raise ValueError(
+            f"Unknown model: {modelname}. Please provide a valid AI Playground model name."
+            "Known models are: " + ", ".join(API_CATALOG_MODELS.keys())
+        )
+
+    return API_CATALOG_MODELS[modelname]
